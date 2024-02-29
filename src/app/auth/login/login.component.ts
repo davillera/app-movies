90e62d1c0f4a59ab105b9bfad9fe7a5352aafd83
Daniel Andrés Villera <davillera77@gmail.com>
2024-02-29 14:17:52 -0500

    add tailwindcss and primeNg

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,12 +1,32 @@
-import { Component } from '@angular/core';
+import {Component, inject, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, ReactiveFormsModule, Validators} from "@angular/forms";
+import {PasswordModule} from "primeng/password";
+import {InputTextModule} from "primeng/inputtext";
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [
+    ReactiveFormsModule,
+    PasswordModule,
+    InputTextModule
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit{
+  public loginForm!: FormGroup;
 
+  private formBuilder = inject(FormBuilder);
+
+  ngOnInit() {
+    this.initFormLogin();
+  }
+
+  initFormLogin() {
+    this.loginForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(8)]]
+    });
+  }
 }
