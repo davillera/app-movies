45ef0c4c0a186d8654230a61edeab857d02adeec
Daniel Andrés Villera <davillera77@gmail.com>
2024-02-29 17:18:28 -0500

    add firebase and deploy

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,14 @@
 import {ApplicationConfig, importProvidersFrom} from '@angular/core';
-import { provideRouter } from '@angular/router';
+import {provideRouter} from '@angular/router';
 
-import { routes } from './app.routes';
+import {routes} from './app.routes';
 import {MessageService} from "primeng/api";
 import {HttpClientModule} from "@angular/common/http";
 import {BrowserModule} from "@angular/platform-browser";
 import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {initializeApp, provideFirebaseApp} from '@angular/fire/app';
+import {getAuth, provideAuth} from '@angular/fire/auth';
+import {getFirestore, provideFirestore} from '@angular/fire/firestore';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -15,6 +18,14 @@ export const appConfig: ApplicationConfig = {
       BrowserModule,
       BrowserAnimationsModule
     ),
-    provideRouter(routes)
+    provideRouter(routes), importProvidersFrom(provideFirebaseApp(() => initializeApp({
+      "projectId": "movie-app-c80bc",
+      "appId": "1:698072277782:web:1e6ac3365d6ff7bb62e637",
+      "databaseURL": "https://movie-app-c80bc-default-rtdb.firebaseio.com",
+      "storageBucket": "movie-app-c80bc.appspot.com",
+      "apiKey": "AIzaSyCBWzn03UE29Mu0xhOHMTnlCW1mVrfAjgs",
+      "authDomain": "movie-app-c80bc.firebaseapp.com",
+      "messagingSenderId": "698072277782"
+    }))), importProvidersFrom(provideAuth(() => getAuth())), importProvidersFrom(provideFirestore(() => getFirestore()))
   ]
 };
---
+++ src/env/environment.ts
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCBWzn03UE29Mu0xhOHMTnlCW1mVrfAjgs",
+  authDomain: "movie-app-c80bc.firebaseapp.com",
+  databaseURL: "https://movie-app-c80bc-default-rtdb.firebaseio.com",
+  projectId: "movie-app-c80bc",
+  storageBucket: "movie-app-c80bc.appspot.com",
+  messagingSenderId: "698072277782",
+  appId: "1:698072277782:web:1e6ac3365d6ff7bb62e637"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
