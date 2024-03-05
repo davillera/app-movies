96286215ddabf8e934fd31a891110058d008dcca
Daniel Andrés Villera <davillera77@gmail.com>
2024-03-05 18:31:03 -0500

    favorite components works

--- src/app/pages/favorites/favorite/favorite.component.ts
+++ src/app/pages/favorites/favorite/favorite.component.ts
@@ -3,12 +3,14 @@ import {SearchComponent} from "../../../shared/header/search/search.component";
 import {AuthService} from "../../../core/services/auth/auth.service";
 import {MoviesService} from "../../../core/services/movies/movies.service";
 import {MessageService} from "primeng/api";
+import {CardModule} from "primeng/card";
 
 @Component({
   selector: 'app-favorite',
   standalone: true,
   imports: [
-    SearchComponent
+    SearchComponent,
+    CardModule
   ],
   templateUrl: './favorite.component.html',
   styleUrl: './favorite.component.scss'
@@ -32,7 +34,7 @@ export class FavoriteComponent implements OnInit {
   getFavorites() {
     this.moviesService.getFavorites().subscribe({
         next: (data: any) => {
-          this.moviesFavorites = data;
+          this.moviesFavorites = data
           console.log(this.moviesFavorites);
         },
         error: (error: any) => {
